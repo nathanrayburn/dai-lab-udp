712f0d4f2665c979a00307fcf40fd0e31d9863d9
nathan <nathanarmand@hotmail.com>
2024-01-30 14:01:35 +0100

    musician

---
+++ musician/src/main/java/udp/Musician.java
@@ -0,0 +1,18 @@
+package udp;
+import java.io.IOException;
+import java.util.HashMap;
+import com.google.gson.*;
+import java.util.logging.Logger;
+
+
+class Musician {
+    final static String IP_ADDR = "239.255.22.5";
+    final static int PORT = 9904;
+    private static final Logger LOG = Logger.getLogger(Musician.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String> instrument = new HashMap<>();
+
+    }
+}
+
